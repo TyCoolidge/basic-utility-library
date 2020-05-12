9f72f156792773d7262a34c27ff5a700b5c7cdc0
TyCoolidge <62319016+TyCoolidge@users.noreply.github.com>
2020-05-12 12:16:41 -0700

    javascript for add, sub, multi, divide, increment

--- functions.js
+++ functions.js
@@ -2,5 +2,28 @@ function add(input1, input2) {
    // A1: any JavaScript value
    // A2: any JavaScript value
    // R: a single JavaScript value
+   //Returns the sum of 3 numbers
+   /* Test for multi line 
+   comments*/
    return input1 + input2;
+}
+
+function subtract(sub1, sub2) {
+   //return should be the difference of the two values
+   return sub1 - sub2;
+}
+
+function multiply(num1, num2, num3) {
+   //return should be the product of the three values
+   return num1 * num2 * num3;
+}
+
+function divide(div1, div2) {
+   //return should be the quotient of the two values
+   return div1 / div2;
+}
+
+function increment(myValue) {
+   //return should add one to value
+   return myValue++;
 }
