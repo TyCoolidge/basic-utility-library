25ce5c480853a2c43bc55e98080eae24ea099572
TyCoolidge <62319016+TyCoolidge@users.noreply.github.com>
2020-05-12 17:03:44 -0700

    working pn push array

--- functions.js
+++ functions.js
@@ -45,19 +45,60 @@ function divDecimal(dec1, dec2, dec3) {
 }
 
 //fix these
-function concatenate(word1) {
+function concatenate(adj1, noun1) {
    //will concatenate the two inputs
 
-   return "The dog is " + word1 + ".";
+   return adj1 + " " + noun1;
 }
 
-function concatenatePlusEquals(word1, word2) {
+function concatenatePlusEquals(name1) {
    //return should concatenate adjective with phrase
 
-   return (word1 += word2);
+   return (name1 += " is the GOAT");
 }
 
 function constructString(fruit) {
    //return should subtract 100 from number typed
    return "My favorite fruit is a/an " + fruit + " because it is delicious.";
 }
+
+function findLength(word1, word2) {
+   //return should show length of each word
+   return word1.length + ", " + word2.length;
+}
+
+function firstLetter(word1, word2, word3, word4) {
+   //will return the first letter of each word
+   return word1[0] + word2[0] + word3[0] + word4[0];
+}
+
+function nthLetter(word1, word2, word3, word4) {
+   //will return the nth letter of each word
+   return word1[3] + " " + word2[4] + " " + word3[2] + " " + word4[1];
+}
+
+function lastLetter(word1) {
+   //will return the last letter in the input
+   return word1[word1.length - 1];
+}
+
+function nthToLastLetter(word1, word2, word3, word4) {
+   //will return the nth letter of each word
+   return (
+      word1[word1.length - 3] +
+      " " +
+      word2[word2.length - 4] +
+      " " +
+      word3[word3.length - 2] +
+      " " +
+      word4[word4.length - 1]
+   );
+}
+
+function pushVar(team1, teams) {
+   //will push input to the create list
+   teams = ["Lakers", "Knicks", "Celtics"];
+   pushVar.push(team1);
+   return;
+}
+//
